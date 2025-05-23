e05f5dcc979a221a1d195f48cbf8c7edbf1b909d
Rithvik <1017rithvik@gmail.com>
2025-05-22 21:26:00 -0500

    user model, auth middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const userModel = require("../models/user");
+
+const userAuth = async (req, res, next) => {
+  try {
+    const cookies = req.cookies;
+    const { token } = cookies;
+    if (!token) {
+      throw new Error("Token invalid or expired");
+    }
+    const decodedObj = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const { _id } = decodedObj;
+    const user = await userModel.findById(_id);
+    if (!user) {
+      throw new Error("User Not Found.");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send(error.message);
+  }
+};
+
+module.exports = userAuth;
---
+++ src/models/user.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 25,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      // will only be called when new document is created.
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+    },
+    about: {
+      type: String,
+      default: "Here you can tell about yourself",
+    },
+    skills: {
+      type: [String],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.methods.getJwtToken = async function () {
+  const user = this;
+  const token = jwt.sign(
+    {
+      _id: user._id,
+    },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: "7d" },
+  );
+  return token;
+};
+
+userSchema.methods.validatePassword = async function (passwordUserInput) {
+  const user = this;
+  const hashedPassword = user.password;
+  const isPasswordValid = await bcrypt.compare(
+    passwordUserInput,
+    hashedPassword,
+  );
+  return isPasswordValid;
+};
+
+const userModel = mongoose.model("User", userSchema);
+module.exports = userModel;
