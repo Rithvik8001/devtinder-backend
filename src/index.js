f9828078ff972a7701b9829ef318316018bcd232
Rithvik <1017rithvik@gmail.com>
2025-05-24 20:46:15 -0500

    cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const { connectDB } = require("./config/database");
 const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 const { authRouter } = require("./routes/auth");
 const { profileRouter } = require("./routes/profile");
 const { requestRouter } = require("./routes/requests");
